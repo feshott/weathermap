fa3e4068da2eb97c783fa274de7747a5dfa4bf70
Yuri Feshott <feshot@yandex.ru>
2018-07-16 23:59:48 +0300

    add server loader/fix app.js/day.js

--- src/App.js
+++ src/App.js
@@ -4,19 +4,17 @@ import Day from './component/day/day';
 import './App.css';
 
 const days = [
-  'Monday',
-  'Tuesday',
-  'Wednesday',
-  'Thursday',
-  'Friday',
-  'Saturday',
-  'Sunday'
+  [0, 1, 2, 3, 4, 5, 6, 7],
+  [8, 9, 10, 11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20, 21, 22, 23],
+  [24, 25, 26, 27, 28, 29, 30, 31],
+  [32, 33, 34, 35, 36, 37, 38, 39]
 ]
 
 class App extends Component {
 
   state = {
-    currState: weather_data,
+    currState: null,
     nowDay: 0
   }
 
@@ -25,20 +23,46 @@ class App extends Component {
     this.setState({ nowDay })
   }
 
+  componentDidMount() {
+    fetch('http://api.openweathermap.org/data/2.5/forecast?q=Moscow,ru&appid=ee6f9118bc7780eab4985a0f5155d14e')
+      .then(
+        (response) => {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' +
+              response.status);
+            return;
+          }
+
+          response.json().then((data) => {
+            this.setState({ currState: data })
+            // console.log(data);
+          });
+        }
+      )
+      .catch(function (err) {
+        console.log('Fetch Error :-S', err);
+      });
+  }
+
   render() {
     const { currState, nowDay } = this.state
+    console.log(currState)
     return (
       <div className="App">
 
-        <div className="weather">
+        {currState != null && <div className="weather">
 
           <div className={`main ${'main_image_' + currState.city.name}`}>
             <div className={`color_weather ${'color_weather_' + currState.list[nowDay].weather[0].main}`}></div>
             <div className="header">
               <p className="city">{currState.city.name}</p>
-              <p key={nowDay}className="curr_day">{days[nowDay]}</p>
+              <p key={nowDay} className="curr_day">
+                {/* получаем название дня недели в зависимости от выбранного
+                 (более подробно в большом описании ниже, в рендере кнопок выбора)*/}
+                {((new Date((currState.list[days[nowDay][0]].dt) * 1000)) + '').slice(0, 4)}
+              </p>
             </div>
-            <Day key={nowDay} date={currState.list[nowDay]} />
+            <Day key={nowDay} date={currState.list} needTime={days[nowDay]}/>
           </div>
 
           <div className="week_wrapper">
@@ -48,14 +72,22 @@ class App extends Component {
                 className="day_btn"
                 data-state={index}
                 onClick={this.setDay}>
-                {elem.substring(0, 3)}
+                {/* В следующей строке я беру первое значение каждого дня,
+                    там использую дату которая в unix формате,
+                    перевожу ее в формат JS,
+                    делаю ее строкой,
+                    беру первые три буквы и получаю название дня которое меняется в зависимости от времени запроса.
+                    мы получаем динамичные кнопки которые меняю название дней ;)
+                   */}
+                {((new Date((currState.list[elem[0]].dt) * 1000)) + '').slice(0, 4)}
+                {/* Похожим способом получаем название погодных условий и получаем картинку*/}
                 <div
-                  className={`day_btn_image ${'day_btn_image_' + currState.list[index].weather[0].main}`}
+                  className={`day_btn_image ${'day_btn_image_' + currState.list[elem[0]].weather[0].main}`}
                   data-state={index} />
               </div>)}
           </div>
 
-        </div>
+        </div>}
 
       </div>
     );
--- src/component/day/day.js
+++ src/component/day/day.js
@@ -3,32 +3,35 @@ import './day.css';
 
 class Day extends Component {
 
-  createTempBlock = (time) => {
+  createTempBlock = (time, timeNum, needTime) => {
     return (
       <div className="temp_block">
         <div className="temp_time">{time}</div>
-        <div className="temp">{Math.round(this.props.date.temp[time] - 273)}°</div>
+        <div className="temp">{Math.round(this.props.date[needTime[timeNum]].main.temp - 273)}°</div>
       </div>)
   }
 
   render() {
-    const { date } = this.props
+    const { date, needTime } = this.props
     console.log(date)
+    console.log(needTime)
     return (
       <div className="day">
 
-        <p className="description">{date.weather[0].description}</p>
+        {/* описание погоды, влажность , скорость ветра, облачность показывают значения середины дня */}
+        <p className="description">{date[needTime[6]].weather[0].description}</p>
 
         <div className="temp_wrapper">
-          {this.createTempBlock('morn')}
-          {this.createTempBlock('day')}
-          {this.createTempBlock('eve')}
-          {this.createTempBlock('night')}
+          {this.createTempBlock('Morn','4', needTime)}
+          {this.createTempBlock('Day','5', needTime)}
+          {this.createTempBlock('Eve','7', needTime)}
+          {this.createTempBlock('Night','2', needTime)}
         </div>
 
-        <p className="speed">{date.speed}mph</p>
-        <p className="clouds">{date.clouds}%</p>
-        <p className="humidity">{date.humidity}%</p>
+        {/* описание погоды, влажность , скорость ветра, облачность показывают значения середины дня */}
+        <p className="speed">{date[needTime[6]].wind.speed}mps</p>
+        <p className="clouds">{date[needTime[6]].clouds.all}%</p>
+        <p className="humidity">{date[needTime[6]].main.humidity}%</p>
       </div>
     );
   }
